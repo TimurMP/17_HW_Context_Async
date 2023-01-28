32f024fdec998db1921b97bff88876cabb8926d5
TimurMP <titimurmamyshev@gmail.commur>
2023-01-28 12:03:44 +0200

    Initial commit

---
+++ contextLost.js
@@ -0,0 +1,39 @@
+let group = {
+    title: 'Java 2022',
+    students: ['Olga', 'Anastasia', 'Sophia'],
+    showList: function (){
+        const show = function (name, title) {
+            console.log(`${title}: ${name}`);
+        }
+        this.students.forEach(s => show(s, this.title));
+    }
+}
+
+
+let group1 = {
+    title: 'Java 2022',
+    students: ['Olga', 'Anastasia', 'Sophia'],
+    showList: function (){
+        const show = function (name) {
+            console.log(`${this.title}: ${name}`);
+        }
+        this.students.forEach(show.bind(this));
+    }
+}
+
+// unmodifiable
+group.showList();
+const newGroup = group;
+group = null;
+console.log('=========================');
+newGroup.showList();
+
+
+console.log('============GROUP1=============');
+
+//unmodifiable
+group1.showList();
+const newGroup1 = group1;
+group1 = null;
+console.log('=========================');
+newGroup1.showList();
